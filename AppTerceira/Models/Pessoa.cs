e6e29dda4e3b01a03cc187769277fe530f23380d
Lucas <lucas.rabaquim@etec.sp.gov.br>
2023-04-16 13:29:43 -0300

    initial commit +git test

---
+++ AppTerceira/Models/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppTerceira.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public char Sexo { get; set; }
+
+        public Pessoa SelectPessoa()
+        {
+            Pessoa ObjPessoa = new Pessoa();
+            return ObjPessoa;
+        }
+    }
+}
