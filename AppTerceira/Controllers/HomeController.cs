7ed6c283ae8d9172ae6cff2ee1b087d24dc3c4e4
Lucas <lucas.rabaquim@etec.sp.gov.br>
2023-04-23 23:42:50 -0300

    Update para a aula do dia 24.04

--- AppTerceira/Controllers/HomeController.cs
+++ AppTerceira/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Mvc;
 using AppTerceira.Models;
@@ -11,21 +12,67 @@ namespace AppTerceira.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            Fisica pf = new Fisica
+            {
+                Id = 2,
+                Nome = "Roberta",
+                Sexo = 'F',
+                CPF = 12345678911,
+                RG = 12345678,
+                Dig_RG = '9'
+            };
+            return View(pf);
         }
 
         public ActionResult About()
         {
             Pessoa pessoa = new Pessoa();
-            pessoa.Id = 12
-            return View();
+            pessoa.Id = 12;
+            pessoa.Nome = "Enildo";
+            pessoa.Sexo = 'F';
+            return View(pessoa);
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            Juridica pj = new Juridica();
+            pj.Id = 1001;
+            pj.Nome = "Fábrica de Gênios";
+            pj.Cnpj = 12345789123412;
+            pj.IE = 123456789;
 
-            return View();
+            return View(pj);
         }
+
+        public ActionResult ExemploTempData()
+        {
+            Juridica pj = new Juridica();
+           
+            pj.Id = 256;
+            pj.Nome = "Desenvolvedores CIA";
+            pj.Cnpj = 12345789188888;
+            pj.IE = 123499999;
+
+            TempData["Id"] = pj.Id;
+            TempData["Nome"] = pj.Nome;
+            TempData["Cnpj"] = pj.Cnpj;
+            TempData["IE"] = pj.IE;
+
+            return RedirectToAction("Contact");
+        }
+
+       /* public ActionResult Pesoa()
+        {
+            Fisica pf = new Fisica
+            {
+                Id = 2,
+                Nome = "Roberta",
+                Sexo = 'F',
+                CPF = 12345678911,
+                RG = 12345678,
+                Dig_RG = '9'
+            };
+            return View(pf);
+        }*/
     }
 }
